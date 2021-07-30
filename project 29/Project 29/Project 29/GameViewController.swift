ae184ef4f904e66f473b19b914909f2f3b1ccce9
Pavel Timofeev <62264409+bkbhhj@users.noreply.github.com>
2021-07-31 02:09:28 +0300

    Add files via upload

---
+++ project 29/Project 29/Project 29/GameViewController.swift
@@ -0,0 +1,114 @@
+//
+//  GameViewController.swift
+//  Project 29
+//
+//  Created by Павел Тимофеев on 28.07.2021.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var angleSlider: UISlider!
+    @IBOutlet weak var angleLabel: UILabel!
+    
+    @IBOutlet weak var velocitySlider: UISlider!
+    @IBOutlet weak var velocityLabel: UILabel!
+    
+    @IBOutlet weak var lauchButton: UIButton!
+    
+    @IBOutlet weak var playerNumber: UILabel!
+    
+    
+    var currentGame: GameScene?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+                
+                currentGame = scene as? GameScene
+                currentGame?.viewController = self
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+        
+        angleChanged(angleSlider)
+        velocityChanged(velocitySlider)
+        
+    }
+    
+    @IBAction func angleChanged(_ sender: UISlider) {
+        angleLabel.text = "Angle: \(Int(angleSlider.value))°"
+        
+    }
+    
+    @IBAction func velocityChanged(_ sender: UISlider) {
+        velocityLabel.text = "Velocity: \(Int(velocitySlider.value))"
+        
+    }
+    
+    @IBAction func launch(_ sender: UIButton) {
+        angleSlider.isHidden = true
+        angleLabel.isHidden = true
+        
+        velocitySlider.isHidden = true
+        velocityLabel.isHidden = true
+        
+        lauchButton.isHidden = true
+        
+        currentGame?.launch(angle: Int(angleSlider.value), velocity: Int(velocitySlider.value))
+        print("asmfkm")
+    }
+    
+    func activatePlayer(number: Int) {
+        if number == 1 {
+            playerNumber.text = "<<< PLAYER ONE"
+        } else {
+            playerNumber.text = "PLAYER TWO >>>"
+        }
+        
+        angleSlider.isHidden = false
+        angleLabel.isHidden = false
+        
+        velocitySlider.isHidden = false
+        velocityLabel.isHidden = false
+        
+        lauchButton.isHidden = false
+        
+    }
+    
+    
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    
+    
+}
