a336f18efa0e9d8604dec1455fb3159e5601a00f
Pavel Timofeev <62264409+bkbhhj@users.noreply.github.com>
2021-07-31 01:46:23 +0300

    Add files via upload

---
+++ Project 19/project 19/project 19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project 19
+//
+//  Created by Павел Тимофеев on 13.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
